ba9e8a153e992ed0599d16ebce59add8f1493c6b
Alex Silva <alex.silva@avioconsulting.com>
2023-06-25 14:25:55 -0300

    validacao

--- maior-divisor-comum/src/worker/mdcCalculator.js
+++ maior-divisor-comum/src/worker/mdcCalculator.js
@@ -1,30 +1,27 @@
 const calcularMDC = (num1, num2) => {
+  if (num1 === undefined || num2 === undefined) {
+    return 0;
+  }
 
-    if (num1 === undefined || num2 === undefined) {
-        return 0
-    }
+  if (num1 === 0 || num2 === 0) {
+    return 0;
+  }
 
-    if (num1 === 0 || num2 === 0) {
-        return 0
-    }
+  console.log("Parâmetros recebidos:", num1, num2);
 
-    console.log('Parâmetros recebidos:', num1, num2);
+  var resto;
 
-    var resto;
+  do {
+    resto = num1 % num2;
+    num1 = num2;
+    num2 = resto;
+  } while (resto !== 0);
 
-    do {
-        resto = num1 % num2;
-        num1 = num2;
-        num2 = resto;
+  return num1;
+};
 
-    } while (resto != 0);
+window.self.addEventListener("message", (event) => {
+  const { num1, num2 } = event.data;
 
-    return num1;
-
-}
-
-window.self.addEventListener('message', (event) => {
-    const { num1, num2 } = event.data;
-
-    calcularMDC(num1, num2);
+  calcularMDC(num1, num2);
 });
